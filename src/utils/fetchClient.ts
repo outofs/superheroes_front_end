685c6426b2759519b801b409f2f45a950fd8adde
outofs <rtranscendental@gmail.com>
2023-09-26 23:30:44 +0300

    Changing base url for server

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://superheroes-tt-api.onrender.com";
-const BASE_URL = "http://127.0.0.1:3000";
+const BASE_URL = "https://superheroes-tt-api.onrender.com";
+// const BASE_URL = "http://127.0.0.1:3000";
 
 export function wait(delay: number) {
   return new Promise(resolve => {
